73bfc73cfa7c0547041f59282b37b223265f1dd6
MariamO6274 <ostatishvili@gmail.com>
2023-07-03 08:14:40 -0600

    Go back to home pg btn

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { useNavigate } from "react-router-dom";
 
 function Dashboard(props) {
+
+    const navigate = useNavigate();
+
+    const goHomeHandel = () => {
+        navigate("/");
+
+    }
   return (
     <div>
         Welcome our website..!!
         Login was successfuly
+        <br/>
+    <button onClick={goHomeHandel}>Home</button>
     </div>
   )
 }
